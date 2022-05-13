d4c55e4bdf73b5348aa057f04ed2cd67e5ed6593
bocheng24 <jeffcgp@hotmail.com>
2022-05-13 17:18:44 +0800

    init project

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='bg-veryDarkBlue'>
+        <div className="container mx-auto flex flex-col-reverse justify-between px-6 py-10 space-y-8 md:flex-row md:space-y-0">
+            
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
